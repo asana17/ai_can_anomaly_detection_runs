3b7439bcda77cb6d505a981abf726599d71565a4
asana17 <kei17.kaight084@gmail.com>
2026-09-16 23:33:57 +0900

    add board/20260916-232708 from quantize/20260916-221145, 24 models

---
+++ board/20260916-232708/nonlinear_ae_k12_h128/network_details.h
@@ -0,0 +1,44 @@
+/**
+  ******************************************************************************
+  * @file    network.h
+  * @date    2026-09-16T23:32:00+0900
+  * @brief   ST.AI Tool Automatic Code Generator for Embedded NN computing
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  ******************************************************************************
+  */
+#ifndef STAI_NETWORK_DETAILS_H
+#define STAI_NETWORK_DETAILS_H
+
+#include "stai.h"
+#include "layers.h"
+
+const stai_network_details g_network_details = {
+  .tensors = (const stai_tensor[7]) {
+   { .size_bytes = 68, .flags = (STAI_FLAG_HAS_BATCH|STAI_FLAG_CHANNEL_LAST), .format = STAI_FORMAT_FLOAT32, .shape = {2, (const int32_t[2]){1, 17}}, .scale = {0, NULL}, .zeropoint = {0, NULL}, .name = "row_output" },
+   { .size_bytes = 512, .flags = (STAI_FLAG_HAS_BATCH|STAI_FLAG_CHANNEL_LAST), .format = STAI_FORMAT_FLOAT32, .shape = {2, (const int32_t[2]){1, 128}}, .scale = {0, NULL}, .zeropoint = {0, NULL}, .name = "_encoder_encoder_0_Gemm_output_0_output" },
+   { .size_bytes = 512, .flags = (STAI_FLAG_HAS_BATCH|STAI_FLAG_CHANNEL_LAST), .format = STAI_FORMAT_FLOAT32, .shape = {2, (const int32_t[2]){1, 128}}, .scale = {0, NULL}, .zeropoint = {0, NULL}, .name = "_encoder_encoder_1_Relu_output_0_output" },
+   { .size_bytes = 48, .flags = (STAI_FLAG_HAS_BATCH|STAI_FLAG_CHANNEL_LAST), .format = STAI_FORMAT_FLOAT32, .shape = {2, (const int32_t[2]){1, 12}}, .scale = {0, NULL}, .zeropoint = {0, NULL}, .name = "_encoder_encoder_2_Gemm_output_0_output" },
+   { .size_bytes = 512, .flags = (STAI_FLAG_HAS_BATCH|STAI_FLAG_CHANNEL_LAST), .format = STAI_FORMAT_FLOAT32, .shape = {2, (const int32_t[2]){1, 128}}, .scale = {0, NULL}, .zeropoint = {0, NULL}, .name = "_decoder_decoder_0_Gemm_output_0_output" },
+   { .size_bytes = 512, .flags = (STAI_FLAG_HAS_BATCH|STAI_FLAG_CHANNEL_LAST), .format = STAI_FORMAT_FLOAT32, .shape = {2, (const int32_t[2]){1, 128}}, .scale = {0, NULL}, .zeropoint = {0, NULL}, .name = "_decoder_decoder_1_Relu_output_0_output" },
+   { .size_bytes = 68, .flags = (STAI_FLAG_HAS_BATCH|STAI_FLAG_CHANNEL_LAST), .format = STAI_FORMAT_FLOAT32, .shape = {2, (const int32_t[2]){1, 17}}, .scale = {0, NULL}, .zeropoint = {0, NULL}, .name = "out_output" }
+  },
+  .nodes = (const stai_node_details[6]){
+    {.id = 1, .type = AI_LAYER_DENSE_TYPE, .input_tensors = {1, (const int32_t[1]){0}}, .output_tensors = {1, (const int32_t[1]){1}} }, /* _encoder_encoder_0_Gemm_output_0 */
+    {.id = 2, .type = AI_LAYER_NL_TYPE, .input_tensors = {1, (const int32_t[1]){1}}, .output_tensors = {1, (const int32_t[1]){2}} }, /* _encoder_encoder_1_Relu_output_0 */
+    {.id = 3, .type = AI_LAYER_DENSE_TYPE, .input_tensors = {1, (const int32_t[1]){2}}, .output_tensors = {1, (const int32_t[1]){3}} }, /* _encoder_encoder_2_Gemm_output_0 */
+    {.id = 4, .type = AI_LAYER_DENSE_TYPE, .input_tensors = {1, (const int32_t[1]){3}}, .output_tensors = {1, (const int32_t[1]){4}} }, /* _decoder_decoder_0_Gemm_output_0 */
+    {.id = 5, .type = AI_LAYER_NL_TYPE, .input_tensors = {1, (const int32_t[1]){4}}, .output_tensors = {1, (const int32_t[1]){5}} }, /* _decoder_decoder_1_Relu_output_0 */
+    {.id = 6, .type = AI_LAYER_DENSE_TYPE, .input_tensors = {1, (const int32_t[1]){5}}, .output_tensors = {1, (const int32_t[1]){6}} } /* out */
+  },
+  .n_nodes = 6
+};
+#endif
+
